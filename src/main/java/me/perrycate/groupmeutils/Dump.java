be29dbfa920a7e9f668f9e5c06b7e708662f8084
Perry Cate <NPCate01@gmail.com>
2016-06-20 11:57:16 -0400

    Update Dump class to not crash at end of group
    Also includes some other changes as to what data we keep (ie now dump
    class just keeps a Group and pulls some info out as needed.)

--- src/main/java/me/perrycate/groupmeutils/Dump.java
+++ src/main/java/me/perrycate/groupmeutils/Dump.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 import me.perrycate.groupmeutils.api.Client;
+import me.perrycate.groupmeutils.data.Group;
 import me.perrycate.groupmeutils.data.GroupMessages;
 import me.perrycate.groupmeutils.data.Message;
 
@@ -14,47 +15,70 @@ import me.perrycate.groupmeutils.data.Message;
 public class Dump {
     private Client groupme;
     private String groupId;
-    private PrintWriter output; // File to dump text to
-    private File outputFolder; // Directory to save images to
+    private String lastMessageId;
+    private Group group;
 
-    public Dump(String token, String id, File file,
-            File folder) {
+    public Dump(String token, Group group) {
         groupme = new Client(token);
-        this.groupId = id;
-        this.outputFolder = folder;
+        this.group = group;
+        this.groupId = group.getId();
+        this.lastMessageId = group.getLastMessageId();
+    }
+
+    /**
+     * Dumps the group to output starting at the specified lastMessageId. The
+     * messages will appear in the file in reverse order, with most recent at
+     * the top.
+     */
+    public void dumpFromBottom(File output) {
+        PrintWriter out = getPrinter(output);
+        GroupMessages messages;
+        messages = groupme.getMessagesBefore(groupId, lastMessageId);
+
+        int totalMessages = messages.getCount();
+        for (int i = 0; i < totalMessages; i += Client.MAX_MESSAGES) {
+            messages = groupme.getMessagesBefore(groupId, lastMessageId);
+            int length = messages.getMessages().length;
+            int j;
+            for (j = 0; j < length; j++) {
+                print(messages.getMessage(j), out);
+            }
+            lastMessageId = messages.getMessage(j - 1).getId();
+        }
+        out.close();
+    }
+
+    /**
+     * Dumps the group to output in order, with most recent messages appearing
+     * at the bottom.
+     */
+    public void dumpFromTop(File output) {
+        // Get each message starting from bottom, write each chunk of 100
+        // messages to an individual file, then concatenate them.
+
+        PrintWriter out = getPrinter(output);
+        GroupMessages messages = groupme.getMessagesBefore(groupId,
+                lastMessageId);
+
+        // Create temporary output directory
+
+        int totalMessages = messages.getCount();
+    }
+
+    private PrintWriter getPrinter(File file) {
 
         try {
-            this.output = new PrintWriter(file);
+            return new PrintWriter(file);
         } catch (FileNotFoundException e) {
             // This should never happen
             System.err.println("FATAL: Failed to open file " + file.getName()
                     + " for writing.");
-            e.printStackTrace();
+            System.exit(1);
+            return null;
         }
     }
 
-    public static void main(String[] args) {
-    }
-
-    /**
-     * Dumps the groupme to output starting at the specified lastMessageId. The
-     * messages will appear in the file in reverse order, with most recent at
-     * the top.
-     */
-    public void dumpFromBottom(String lastMessageId) {
-        GroupMessages messages;
-        while (true) {
-            messages = groupme.getMessagesBefore(groupId, lastMessageId);
-            int length = messages.getMessages().length;
-            int i;
-            for (i = 0; i < length; i++) {
-                print(messages.getMessage(i));
-            }
-            lastMessageId = messages.getMessage(i - 1).getId();
-        }
-    }
-
-    public void print(Message message) {
+    private void print(Message message, PrintWriter output) {
         if (message.getText() == null) {
             output.println(message.getId() + " | "
                     + message.getCreatedAt() + " | "
